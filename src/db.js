8954a6902f274705b8cf9420a8aa1a7f2a0fd655
jerrenss <>
2020-11-27 22:40:16 +0800

    Added verification for database connection

--- src/db.js
+++ src/db.js
@@ -1,13 +1,21 @@
-const Pool = require('pg').Pool
+const { Client } = require('pg')
 const dotenv = require('dotenv')
 dotenv.config()
 
-const pool = new Pool({
+const pool = new Client({
   user: process.env.POSTGRESQL_USER,
   password: process.env.POSTGRESQL_PASSWORD,
   host: process.env.POSTGRESQL_HOST,
   port: process.env.POSTGRESQL_PORT,
   database: process.env.POSTGRESQL_DATABASE,
+})
+
+pool.connect((err) => {
+  if (!err) {
+    console.log('PostgreSQL database is connected!')
+  } else {
+    console.log('PostgreSQL database failed to connect!')
+  }
 })
 
 module.exports = pool
